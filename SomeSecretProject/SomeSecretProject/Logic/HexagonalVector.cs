fc194be2a4dcbd3b5bb081dcebdacd5c3ac77cfe
MichaelEk <michael.khr@gmail.com>
2015-08-07 19:15:41 +0500

    HexVector

---
+++ SomeSecretProject/SomeSecretProject/Logic/HexagonalVector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeSecretProject.Logic
+{
+	class HexagonalVector
+	{
+		public int X;
+		public int Y;
+
+		public HexagonalVector(int inGameX, int inGameY)
+		{
+
+		}
+	}
+}
