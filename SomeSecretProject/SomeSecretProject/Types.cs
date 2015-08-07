b6213a7e132d48e585d3a7d1467ecafe5d6490a1
kseniazhagorina <ksenia.zhagorina@yandex.ru>
2015-08-07 18:42:01 +0500

    Common types

---
+++ SomeSecretProject/SomeSecretProject/Types.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeSecretProject
+{
+    class Cell
+    {
+        public int x;
+        public int y;
+    }
+
+    class Unit
+    {
+        public Cell[] members;
+        public Cell pivot;
+
+    }
+}
