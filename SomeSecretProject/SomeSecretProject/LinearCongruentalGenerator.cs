58fad62e83d5aaea1cfb479735249f8dde9a85f4
unknown <khr@khr2.kontur>
2015-08-07 18:00:43 +0500

    random generator added

---
+++ SomeSecretProject/SomeSecretProject/LinearCongruentalGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeSecretProject
+{
+	class LinearCongruentalGenerator
+	{
+		private const long Modulus = 1 << 32;
+		private const long Multiplier = 1103515245;
+		private const long Increment = 12345;
+
+		private long CurrentState = 0;
+		public int Seed { get; private set; }
+
+		public LinearCongruentalGenerator(int seed)
+		{
+			Seed = seed;
+		}
+
+		public int GetNext()
+		{
+			CurrentState = (Multiplier*CurrentState + Increment)%Modulus; 
+			return (int)CurrentState;
+		}
+	}
+}
