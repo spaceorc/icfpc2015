9f6624274ab6ba23da87c1d2a87ef9d632427275
spaceorc <spaceorc@skbkontur.ru>
2015-08-08 12:47:02 +0500

    reachable positions done

--- SomeSecretProject.Tests/ReachablePositions_Test.cs
+++ SomeSecretProject.Tests/ReachablePositions_Test.cs
@@ -22,93 +22,27 @@ namespace SomeSecretProject.Tests
 .....");
 			var unit = "{pivot:{x:1,y:0},members:[{x:1,y:0}]}".ParseAsJson<Unit>();
 			var reacher = new ReachablePositions(map);
-			var expected = new[]
-			               {GetUnit(@"
-@..##
-.....
-##...
-.#...
-....."),
-							GetUnit(@"
-..@##
-.....
-##...
-.#...
-....."),
-							GetUnit(@"
-...##
-@....
-##...
-.#...
-....."),
-							GetUnit(@"
-...##
-.@...
-##...
-.#...
-....."),
-							GetUnit(@"
-...##
-....@
-##...
-.#...
-....."),
-							GetUnit(@"
-...##
-.....
-##@..
-.#...
-....."),
-							GetUnit(@"
-...##
-.....
-##..@
-.#...
-....."),
-							GetUnit(@"
-...##
-.....
-##...
-.#@..
-....."),
-							GetUnit(@"
-...##
-.....
-##...
-.#..@
-....."),
-							GetUnit(@"
-...##
-.....
-##...
-.#...
-@...."),
-							GetUnit(@"
-...##
-.....
-##...
-.#...
-.@..."),
-							GetUnit(@"
-...##
-.....
-##...
-.#...
-..@.."),
-							GetUnit(@"
-...##
-.....
-##...
-.#...
-...@."),
-							GetUnit(@"
-...##
-.....
-##...
-.#...
-....@"),
-			               };
+			var expected = @"
+				[{pivot: {x: 0, y: 0}, members: [{x: 0, y: 0}]},
+				{pivot: {x: 2, y: 0}, members: [{x: 2, y: 0}]},
+				{pivot: {x: 0, y: 1}, members: [{x: 0, y: 1}]},
+				{pivot: {x: 1, y: 1}, members: [{x: 1, y: 1}]},
+				{pivot: {x: 4, y: 1}, members: [{x: 4, y: 1}]},
+				{pivot: {x: 2, y: 2}, members: [{x: 2, y: 2}]},
+				{pivot: {x: 4, y: 2}, members: [{x: 4, y: 2}]},
+				{pivot: {x: 2, y: 3}, members: [{x: 2, y: 3}]},
+				{pivot: {x: 4, y: 3}, members: [{x: 4, y: 3}]},
+				{pivot: {x: 0, y: 4}, members: [{x: 0, y: 4}]},
+				{pivot: {x: 1, y: 4}, members: [{x: 1, y: 4}]},
+				{pivot: {x: 2, y: 4}, members: [{x: 2, y: 4}]},
+				{pivot: {x: 3, y: 4}, members: [{x: 3, y: 4}]},
+				{pivot: {x: 4, y: 4}, members: [{x: 4, y: 4}]}]".ParseAsJson<Unit[]>();
 			var actual = reacher.EndPositions(unit).Select(t => t.Item1).OrderBy(u => Tuple.Create(u.pivot.y, u.pivot.x));
+			foreach (var u in actual)
+			{
+				Console.WriteLine(u);
+				Console.WriteLine();
+			}
 			CollectionAssert.AreEqual(expected, actual.ToArray());
 		}
 
@@ -122,49 +56,23 @@ namespace SomeSecretProject.Tests
 ##..
 .#..
 ....");
-			var unit = GetUnit(@"
-+
-..*
-**
-");
+			var unit = "{pivot: {x: 0, y: -1}, members: [{x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}]}".ParseAsJson<Unit>();
 			var reacher = new ReachablePositions(map);
-			var expected = new[]
-			               {GetUnit(@"
-+
-..*.
-**..
-##..
-.#..
-...."),
-	 GetUnit(@"
- +
-...*
-.**.
-##..
-.#..
-...."),
-	 GetUnit(@"
- +
-*...
-**..
-##..
-.#..
-...."),
-	 GetUnit(@"
-  +
-.*..
-.**.
-##..
-.#..
-...."),
-	 GetUnit(@"
-   +
-..*.
-..**
-##..
-.#..
-...."),
-			               };
+			var expected =
+				@"[{pivot: {x: 0, y: -1}, members: [{x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}]},
+				{pivot: {x: 1, y: -1}, members: [{x: 3, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}]},
+				{pivot: {x: 1, y: -1}, members: [{x: 1, y: 1}, {x: 0, y: 0}, {x: 0, y: 1}]},
+				{pivot: {x: 2, y: -1}, members: [{x: 2, y: 1}, {x: 1, y: 0}, {x: 1, y: 1}]},
+				{pivot: {x: 3, y: -1}, members: [{x: 3, y: 1}, {x: 2, y: 0}, {x: 2, y: 1}]},
+				{pivot: {x: 2, y: 0}, members: [{x: 3, y: 1}, {x: 2, y: 2}, {x: 3, y: 2}]},
+				{pivot: {x: 3, y: 0}, members: [{x: 3, y: 2}, {x: 1, y: 1}, {x: 2, y: 2}]},
+				{pivot: {x: 3, y: 1}, members: [{x: 3, y: 3}, {x: 2, y: 2}, {x: 2, y: 3}]},
+				{pivot: {x: 3, y: 1}, members: [{x: 2, y: 2}, {x: 2, y: 0}, {x: 1, y: 1}]},
+				{pivot: {x: 4, y: 1}, members: [{x: 3, y: 2}, {x: 3, y: 0}, {x: 2, y: 1}]},
+				{pivot: {x: 4, y: 2}, members: [{x: 2, y: 3}, {x: 2, y: 1}, {x: 2, y: 2}]},
+				{pivot: {x: 5, y: 2}, members: [{x: 3, y: 3}, {x: 3, y: 1}, {x: 3, y: 2}]},
+				{pivot: {x: 4, y: 3}, members: [{x: 3, y: 4}, {x: 3, y: 2}, {x: 2, y: 3}]}]".ParseAsJson<Unit[]>();
+			
 			var actual = reacher.EndPositions(unit).Select(t => t.Item1).OrderBy(u => Tuple.Create(u.pivot.y, u.pivot.x));
 			foreach (var u in actual)
 			{
@@ -193,71 +101,6 @@ namespace SomeSecretProject.Tests
 				}
 			}
 			return map;
-		}
-
-		public Unit GetUnit(string source)
-		{
-			var rows = source.Split(new []{Environment.NewLine}, StringSplitOptions.None)
-				.SkipWhile(String.IsNullOrWhiteSpace).Reverse()
-				.SkipWhile(String.IsNullOrWhiteSpace).Reverse().ToList();
-			Cell pivot = null;
-			var cells = new List<Cell>();
-			var width = rows.Where(x => x.Trim().Length > 1).Min(x => x.Trim().Length);
-			if (rows[0].Trim().Length == 1)
-			{
-				pivot = new Cell {x = rows[0].IndexOf('+') - rows.First(x => x.Trim().Length > 1).TakeWhile(c => c == ' ').Count(), 
-					y = -rows.TakeWhile(r => r.Trim().Length <= 1).Count()};
-				rows = rows.SkipWhile(r => r.Trim().Length <= 1).ToList();
-			}
-			if (rows.Last().Trim().Length == 1)
-			{
-				pivot = new Cell {x = rows[0].IndexOf('+') - rows.First(x => x.Trim().Length > 1).TakeWhile(c => c == ' ').Count(),
-					y = rows.Count - 1};
-				rows = rows.TakeWhile(r => r.Trim().Length > 1).ToList();
-			}
-			for (int y = 0; y < rows.Count; y++)
-			{
-				var row = rows[y].Trim();
-				if (row.Length > width)
-				{
-					if (row[0] == '+')
-					{
-						pivot = new Cell
-						{
-							x = -row.TakeWhile(c => c == '+' || c == ' ').Count(),
-							y = y
-						};
-						row = row.Substring(1).Trim();
-					}
-					if (row.Last() == '+')
-					{
-						pivot = new Cell
-						{
-							x = row.Length - 1,
-							y = y
-						};
-						row = row.Substring(0, width);
-					}
-				}
-				for (int x = 0; x < row.Length; x++)
-				{
-					if (row[x] == '*')
-					{
-						cells.Add(new Cell { x = x, y = y });
-					}
-					else if (row[x] == '@')
-					{
-						pivot = new Cell { x = x, y = y };
-						cells.Add(pivot);
-					}
-					else if (row[x] == '+')
-					{
-						pivot = new Cell { x = x, y = y };
-					}
-				}
-
-			}
-			return new Unit{pivot = pivot, members = new HashSet<Cell>(cells.ToArray())};
 		}
 	}
 }
