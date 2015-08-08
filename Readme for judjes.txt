041b46dc8e6396f490acf2b027fe07581885be53
pesterev da <pesterev_da@skbkontur.ru>
2015-08-08 15:30:40 +0500

    Pack er

---
+++ Readme for judjes.txt
@@ -0,0 +1,10 @@
+Ei!
+
+Our solution is written in C# on .NET 4.5.
+To build it you need to run 'buildall.cmd', output exe file will be here: 'SomeSecretProject\bin\Release\play_icfp2015.exe'
+
+Probable problems:
+If you don't have .NET framework 4.5, you can download it here: https://www.microsoft.com/ru-ru/download/details.aspx?id=30653
+
+Used libs:
+Newtonsoft.Json.dll - downloaded from https://github.com/JamesNK/Newtonsoft.Json/releases/download/7.0.1/Json70r1.zip (\Bin\New45)
---
+++ buildall.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal ENABLEEXTENSIONS
+pushd %~dp0
+
+FOR /F "usebackq skip=2 tokens=1-3" %%A IN (`reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath 2^>nul`) DO (
+    set ValueName=%%A
+    set ValueType=%%B
+    set ValueValue=%%C
+)
+if not defined ValueValue (
+	echo Need framework 4.5! Ei!
+	goto :EOF
+)
+
+%ValueValue%\msbuild.exe /m SomeSecretProjectCore.sln /property:Configuration=Release
+
+echo Run SomeSecretProject/bin/release/play_icfp2015.exe
+
+popd
+pause
