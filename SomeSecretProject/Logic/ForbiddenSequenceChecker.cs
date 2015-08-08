7e89f3964adcfd17649b0a62eb3c4fc07553198e
spaceorc <spaceorc@skbkontur.ru>
2015-08-08 12:14:58 +0500

    fix ForbiddenSequenceChecker

--- SomeSecretProject/Logic/ForbiddenSequenceChecker.cs
+++ SomeSecretProject/Logic/ForbiddenSequenceChecker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
@@ -28,31 +29,56 @@ namespace SomeSecretProject.Logic
 		{
 			if (lastMove == MoveType.NW || lastMove == MoveType.NE)
 				return false;
-			var prevMove = moves.Any() ? moves.Last() : (MoveType?)null;
-			if (lastMove == MoveType.E && prevMove == MoveType.W)
-				return false;
-			if (lastMove == MoveType.W && prevMove == MoveType.E)
-				return false;
-			if (lastMove == MoveType.RotateCCW && (symmetric == 1 || prevMove == MoveType.RotateCW))
-				return false;
-			if (lastMove == MoveType.RotateCW && (symmetric == 1 || prevMove == MoveType.RotateCCW))
-				return false;
-			if (lastMove == MoveType.RotateCCW || lastMove == MoveType.RotateCW)
+			if (lastMove == MoveType.SW || lastMove == MoveType.SE)
+				return true;
+			if (symmetric == 1)
 			{
-				var rotateCount = 1;
-				for (int i = moves.Count - 1; i >= 0; --i)
-				{
-					var prevMoveType = moves[i];
-					if (prevMoveType == lastMove)
-					{
-						if (symmetric == ++rotateCount)
-							return false;
-					}
-					else
-						break;
-				}
+				if (lastMove == MoveType.RotateCCW || lastMove == MoveType.RotateCW)
+					return false;
+				var prevMove = moves.Any() ? moves.Last() : (MoveType?)null;
+				if (lastMove == MoveType.E && prevMove == MoveType.W)
+					return false;
+				if (lastMove == MoveType.W && prevMove == MoveType.E)
+					return false;
+				return true;
+			}
+			var used = new HashSet<Tuple<int, int>>();
+			var position = Tuple.Create(0, 0);
+			used.Add(position);
+			position = ApplyMove(lastMove, position);
+			if (position == null)
+				throw new InvalidOperationException("Position couldn't be null here");
+			used.Add(position);
+			for (var i = moves.Count - 1; i >= 0; i--)
+			{
+				position = ApplyMove(moves[i], position);
+				if (position == null)
+					return true;
+				if (!used.Add(position))
+					return false;
 			}
 			return true;
+		}
+
+		[CanBeNull]
+		private Tuple<int, int> ApplyMove(MoveType moveType, [NotNull] Tuple<int, int> position)
+		{
+			switch (moveType)
+			{
+				case MoveType.E:
+					return Tuple.Create(position.Item1 + 1, position.Item2);
+				case MoveType.W:
+					return Tuple.Create(position.Item1 - 1, position.Item2);
+				case MoveType.SW:
+				case MoveType.SE:
+					return null;
+				case MoveType.RotateCW:
+					return Tuple.Create(position.Item1, (position.Item2 + 1) % symmetric);
+				case MoveType.RotateCCW:
+					return Tuple.Create(position.Item1, (position.Item2 + symmetric - 1) % symmetric);
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
 		}
 	}
 }
