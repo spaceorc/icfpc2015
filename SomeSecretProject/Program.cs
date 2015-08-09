3649f7d0f4793efe24acecbea6a1e3ea1742ca94
pesterev da <pesterev_da@skbkontur.ru>
2015-08-09 19:51:45 +0500

    Множественный флаг f

--- SomeSecretProject/Program.cs
+++ SomeSecretProject/Program.cs
@@ -11,13 +11,13 @@ namespace SomeSecretProject
         private static readonly Dictionary<string, Action<InputParameters, string>> actions = new Dictionary<string, Action<InputParameters, string>>
         {
             {"-f", ParseFilename},
-            {"-p", ParsePower},//todo
+            {"-p", ParsePower},
             {"-t", ParseTL},
             {"-m", ParseML},
             {"-c", ParseCores}
         };
 
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
             var argsQueue = new Queue<string>(args);
 		    var inputParameters = new InputParameters();
@@ -28,34 +28,38 @@ namespace SomeSecretProject
 		            var keyValue = ParseNextArg(argsQueue);
 		            actions[keyValue.Item1](inputParameters, keyValue.Item2);
 		        }
-                if (inputParameters.InputFilename == null)
+                if (inputParameters.InputFilenames == null || inputParameters.InputFilenames.Count < 1)
                     throw new Exception("MUST provide us with -f");
 		    }
 		    catch (Exception ex)
 		    {
-		        Console.WriteLine("Error: " + ex.Message);
+		        Console.WriteLine("Error: " + ex.ToString());
                 Environment.Exit(111);
 		    }
-            var problem = File.ReadAllText(inputParameters.InputFilename).ParseAsJson<Problem>();
-            var outputs = new List<Output>();
-		    var problemSolver = ProblemSolverFactory.GetSolver();
-		    foreach (var seed in problem.sourceSeeds)
+
+		    var outputs = new List<Output>();
+		    foreach (var fn in inputParameters.InputFilenames)
 		    {
-		        var answer = problemSolver.Solve(problem, seed, inputParameters.PowerPhrases.ToArray());
-		        outputs.Add(new Output
+		        var problem = File.ReadAllText(fn).ParseAsJson<Problem>();
+		        var problemSolver = ProblemSolverFactory.GetSolver();
+		        foreach (var seed in problem.sourceSeeds)
 		        {
-		            problemId = problem.id,
-                    seed = seed,
-                    tag = DateTime.Now.ToString("O"),
-                    solution = answer
-		        });
+		            var answer = problemSolver.Solve(problem, seed, inputParameters.PowerPhrases.ToArray());
+		            outputs.Add(new Output
+		            {
+		                problemId = problem.id,
+		                seed = seed,
+		                tag = DateTime.Now.ToString("O"),
+		                solution = answer
+		            });
+		        }
 		    }
 		    Console.Write(outputs.ToJson()); //too big strng?
 		}
 
 	    private static void ParseFilename(InputParameters input, string value)
 	    {
-	        input.InputFilename = value;
+            input.InputFilenames.Add(value);
 	    }
 
         private static void ParsePower(InputParameters input, string value)
@@ -97,12 +101,13 @@ namespace SomeSecretProject
             PowerPhrases = new List<string>();
             TimeLimitSec = 60;
             MemoryLimitMb = 1024;
+            InputFilenames = new List<string>();
         }
 
         public int Cores { get; set; }
-        public string InputFilename { get; set; }
+        public IList<string> InputFilenames { get; set; }
         public int MemoryLimitMb { get; set; }
-        public int TimeLimitSec { get; set; } //todo is for all seeds or for each?
+        public int TimeLimitSec { get; set; }
         public IList<string> PowerPhrases { get; set; }
     }
 }
